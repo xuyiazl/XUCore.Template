1f03537310a72ec11be7a2336854fbbc09a244d1
xuyiazl <3624091@qq.com>
2021-12-29 16:08:48 +0800

    删掉doman

--- XUCore.Template.Mediator2/Content/XUCore.Template.Mediator2/XUCore.Template.Mediator2.Domain/User/Menu/Dtos/MenuTreeDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XUCore.Template.Mediator2.Domain;
-
-/// <summary>
-/// 导航列表
-/// </summary>
-public class MenuTreeDto : MenuDto
-{
-    public IList<MenuTreeDto> Childs { get; set; }
-}
--- XUCore.Template.Mediator2/Content/XUCore.Template.Mediator2/XUCore.Template.Mediator2.Domain/User/User/Dtos/LoginTokenDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XUCore.Template.Mediator2.Domain;
-
-public class LoginTokenDto
-{
-    public string Token { get; set; }
-    public long Expires { get; set; }
-}
