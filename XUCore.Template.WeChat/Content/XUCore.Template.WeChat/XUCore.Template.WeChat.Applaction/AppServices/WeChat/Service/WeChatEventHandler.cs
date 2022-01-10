f8dbdf3d3c887b8139b40fce2b1918725162f3be
Nigel <3624091@qq.com>
2022-01-10 12:57:53 +0800

    1

--- XUCore.Template.WeChat/Content/XUCore.Template.WeChat/XUCore.Template.WeChat.Applaction/AppServices/WeChat/Service/WeChatEventHandler.cs
+++ XUCore.Template.WeChat/Content/XUCore.Template.WeChat/XUCore.Template.WeChat.Applaction/AppServices/WeChat/Service/WeChatEventHandler.cs
@@ -16,43 +16,88 @@ public class WeChatEventsHandler : IWxEventsHandler
     /// <returns></returns>
     public async Task<ToMessageBase> Execute(IFromMessage fromMessage)
     {
-        if (fromMessage is FromTextMessage)
+        if (fromMessage is FromSubscribeEvent)
         {
-            //返回多图文
-            var toMsg = new ToNewsMessage()
-            {
-                Articles = new List<ToNewsMessage.ArticleInfo>()
-                {
-                    new ToNewsMessage.ArticleInfo()
-                    {
-                        Description = "最简洁最易于使用的微信Sdk，包括公众号Sdk、小程序Sdk、企业微信Sdk等，以及Abp VNext集成。",
-                        PicUrl = "https://www.xin-lai.com/imgs/xinlai-logo_9d2c29c2794e6a173738bf92b056ab69.png",
-                        Title="Magicodes.Wx.Sdk简介",
-                        Url = "http://xin-lai.com"
-                    }
-                },
-                FromUserName = "Test",
-                ToUserName = "Test"
-            };
-            return await Task.FromResult(toMsg);
-        }
-        else if (fromMessage is FromSubscribeEvent)
-        {
-            //返回文本消息
             return await Task.FromResult(new ToTextMessage()
             {
                 Content = "欢迎关注!",
             });
         }
+        else if (fromMessage is FromUnsubscribeEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromScanEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromLocationEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromLocationEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromClickEvent)
+        {
+            var clickEvent = fromMessage as FromClickEvent;
+
+            switch (clickEvent.EventKey)
+            {
+                case "V1001_GOOD":
+                    return await Task.FromResult(new ToTextMessage()
+                    {
+                        Content = "欢迎关注!",
+                    });
+                default:
+                    return await Task.FromResult(new ToTextMessage()
+                    {
+                        Content = "欢迎关注!",
+                    });
+            }
+        }
+        else if (fromMessage is FromViewEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromViewMiniprogramEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
+        else if (fromMessage is FromTemplateSendJobFinishEvent)
+        {
+            return await Task.FromResult(new ToNullMessage());
+        }
         else if (fromMessage is FromTextMessage)
         {
-            //返回文本
             return await Task.FromResult(new ToTextMessage()
             {
-                Content = "Test",
+                Content = "欢迎关注!",
             });
         }
-        return await Task.FromResult(new ToNullMessage());
+        //else if (fromMessage is FromTextMessage)
+        //{
+        //    //返回多图文
+        //    var toMsg = new ToNewsMessage()
+        //    {
+        //        Articles = new List<ToNewsMessage.ArticleInfo>()
+        //        {
+        //            new ToNewsMessage.ArticleInfo()
+        //            {
+        //                Description = "最简洁最易于使用的微信Sdk，包括公众号Sdk、小程序Sdk、企业微信Sdk等，以及Abp VNext集成。",
+        //                PicUrl = "https://www.xin-lai.com/imgs/xinlai-logo_9d2c29c2794e6a173738bf92b056ab69.png",
+        //                Title="Magicodes.Wx.Sdk简介",
+        //                Url = "http://xin-lai.com"
+        //            }
+        //        },
+        //        FromUserName = "Test",
+        //        ToUserName = "Test"
+        //    };
+        //    return await Task.FromResult(toMsg);
+        //}
+        else
+            return await Task.FromResult(new ToNullMessage());
     }
 }
 
